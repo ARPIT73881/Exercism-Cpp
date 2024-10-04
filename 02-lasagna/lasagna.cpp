b61c16d2ff336c048f9ee8f6b6d35be75640b8e1
Arpit Sharma <AS.73881@GMAIL.COM>
2024-10-04 22:30:36 +0530

    complete

--- 02-lasagna/lasagna.cpp
+++ 02-lasagna/lasagna.cpp
@@ -1,14 +1,17 @@
 // ovenTime returns the amount in minutes that the lasagna should stay in the
 // oven.
-int ovenTime() {
+int ovenTime()
+{
     // TODO: Return the correct time.
+    // return the value 40
     return 40;
 }
 
 /* remainingOvenTime returns the remaining
    minutes based on the actual minutes already in the oven.
 */
-int remainingOvenTime(int actualMinutesInOven) {
+int remainingOvenTime(int actualMinutesInOven)
+{
     // TODO: Calculate and return the remaining in the oven based on the time
     // the lasagna has already been there.
     return ovenTime() - actualMinutesInOven;
@@ -17,7 +20,8 @@ int remainingOvenTime(int actualMinutesInOven) {
 /* preparationTime returns an estimate of the preparation time based on the
    number of layers and the necessary time per layer.
 */
-int preparationTime(int numberOfLayers) {
+int preparationTime(int numberOfLayers)
+{
     // TODO: Calculate and return the preparation time with the
     // `numberOfLayers`.
     const int timePerLayer = 2;
@@ -26,7 +30,8 @@ int preparationTime(int numberOfLayers) {
 
 // elapsedTime calculates the total time spent to create and bake the lasagna so
 // far.
-int elapsedTime(int numberOfLayers, int actualMinutesInOven) {
+int elapsedTime(int numberOfLayers, int actualMinutesInOven)
+{
     // TODO: Calculate and return the total time so far.
     return preparationTime(numberOfLayers) + actualMinutesInOven;
 }
